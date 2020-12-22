01bc5780a9a8a89179274f6b0512dd842c9715a3
Giuseppe Setem <giuseppe7d@gmail.com>
2020-12-22 14:31:39 +0000

    feat(render-canvas): add orbital camera

--- src/pages/RenderCanvas.tsx
+++ src/pages/RenderCanvas.tsx
@@ -1,46 +1,22 @@
-import React, { Suspense, useEffect, useMemo } from "react";
+import React, { Suspense } from "react";
 import { useParams } from "react-router-dom";
-import { Canvas, useFrame, useLoader } from "react-three-fiber";
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-const BASE_URL = "https://storage.googleapis.com/ar-manual.appspot.com";
-
-interface IGLTFModelProps {
-  folder: string;
-  file: string;
-}
-
-const GLTFModel: React.FC<IGLTFModelProps> = ({ folder, file }) => {
-  const mesh = useLoader(GLTFLoader, `${BASE_URL}/${folder}/${file}`);
-  const mixer = useMemo(() => new THREE.AnimationMixer(mesh.scene), [mesh]);
-  const clips = useMemo(() => mesh.animations, [mesh]);
-
-  useEffect(() => {
-    if (clips && mixer && mesh) {
-      clips.forEach((clip) => mixer.clipAction(clip).setDuration(5).play());
-    }
-  }, [mesh, clips, mixer]);
-
-  useFrame((state, delta) => {
-    mixer.update(delta);
-  });
-
-  return (
-    <mesh position={[0, 0, 0]}>
-      <primitive object={mesh.scene} />
-    </mesh>
-  );
-};
+import { Canvas } from "react-three-fiber";
+import { GLTFModel } from "../components/container/3D/GLTFModel";
+import { OrbitCamera } from "../components/container/3D/OrbitCamera";
 
 export const RenderCanvas = () => {
   const params: { folder: string; file: string } = useParams();
 
   return (
     <Canvas
-      style={{ flex: 1 }}
-      camera={{ position: [0, 6, 7], near: 5, far: 15 }}
+      style={{ flex: 1, backgroundColor: "#333" }}
+      camera={{ position: [0, 6, 7] }}
+      gl={{
+        antialias: true,
+        logarithmicDepthBuffer: true,
+      }}
     >
+      <OrbitCamera />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
