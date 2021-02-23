7dfba5304411467b851da2b047d064e84c6d4c0e
Giuseppe Setem <giuseppe7d@gmail.com>
2021-02-23 09:39:15 +0000

    fix: add revalidation after operations

--- src/hooks/useStatistics.ts
+++ src/hooks/useStatistics.ts
@@ -4,12 +4,13 @@ import { Statistics } from "../models/Statistics";
 import { fetcher } from "../services/api";
 
 export const useStatistics = (testbenchId: string) => {
-  const { data, error } = useSWR(
+  const { data, error, revalidate } = useSWR(
     `/testbenches/${testbenchId}/statistics`,
     fetcher
   );
 
   return {
+    revalidate,
     statistics: data as Statistics,
     isLoading: !error && !data,
     isError: error,
